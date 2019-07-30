9758bf6807f5da4cba70b869b47231f6f3d09ca4
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-07-30 13:13:59 -0700

    finished isBst

--- drills.js
+++ drills.js
@@ -13,7 +13,7 @@ function main() {
   BST.insert(2, null);
   BST.insert(5, null);
   BST.insert(7, null);
-  // console.log(BST);
+  console.log(BST);
   // BST.printValues(BST);
   const BSTEasy = new BinarySearchTree();
   BSTEasy.insert('E', null);
@@ -31,7 +31,76 @@ function main() {
 
 }
 
-main();
+// main();
+
+
+// [3, 2, 4, 1, 6,5];
+
+function tree(t) {
+  if (!t) {
+    return 0;
+  }
+  return tree(t.left) + t.value + tree(t.right);
+}
+
+const bst = new BinarySearchTree();
+bst.insert(4, 4);
+bst.insert(2, 2);
+bst.insert(3, 3);
+bst.insert(1, 1);
+bst.insert(6, 6);
+bst.insert(5, 5);
+bst.insert(7, 7);
+bst.insert(8, 8);
+
+const notABinary = [1,2,3,4]
+
+notABinary.left = 6;
+notABinary.right = 2;
+
+
+// console.log(tree(bst))
+
+
+function maxHeight(binaryTree) {
+  if (!binaryTree) {
+    return 0;
+  } else {
+    let lHeight = maxHeight(binaryTree.left);
+    let rHeight = maxHeight(binaryTree.right);
+    if (lHeight > rHeight) {
+      return lHeight + 1;
+    } else {
+      return rHeight + 1;
+    }
+  }
+}
+
+// console.log(maxHeight(bst));
+
+function isBst(binaryTree, min, max) {
+  if (min !== undefined && binaryTree.key < min) {
+    return false;
+  }
+
+  if (max !== undefined && binaryTree.key > max) {
+    return false;
+  }
+
+  if (binaryTree.left && !isBst(binaryTree.left, min, binaryTree.key)) {
+    return false;
+  }
+
+  if (binaryTree.right && !isBst(binaryTree.right, binaryTree.key, max)) {
+    return false;
+  }
+
+  return true;
+  
+}
+
+console.log(isBst(bst, Number.MIN_VALUE, Number.MAX_VALUE));
+
 
 // # 1-2
 /* Drawing tree structures */
